09d1904f9e1b900737fb89cb0f724ed2c85a08f6
aneelaanum-collab <aneela.anum.gondle@gmail.com>
2025-10-01 10:20:57 +0200

    test 2

---
+++ js/app.js
@@ -0,0 +1,10 @@
+console.log(
+  "Hello world"
+);
+console.log("This is line 1.");
+console.log("This is line 2.");
+console.log("This is line 3."); console.log("This is line 4.")
+;
+console.log("This is line 5.");;
+;
+console.log("This is line 6.");
